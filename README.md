024cc0ee3072322ca713c7cb51430431530502a4
Dark-SHADOW-OFFICIAL <221742868+Mr-Marco09@users.noreply.github.com>
2026-02-21 22:54:24 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Dark_mini_md
-Bot whatsapp Bailey's
+# Dark-mini-md bot whatsapp
+
---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Hub/shadow.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
